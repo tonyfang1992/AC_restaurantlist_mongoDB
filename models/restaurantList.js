24f660f92498460e8f8f6260b33b096471c8beeb
tonyfang <q710370@gmail.com>
2019-11-16 18:54:45 +0800

    Add restaurantlistSeeder

---
+++ models/restaurantList.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const restaurantlistSchema = new Schema({
+  id: {
+    type: String,             // 資料型別是字串
+    required: true            // 這是個必填欄位
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  name_en: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  google_map: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('restaurantList', restaurantlistSchema)
