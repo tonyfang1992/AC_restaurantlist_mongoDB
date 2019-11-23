fd89b685849c5445631f4f48f1617ce114767f66
tonyfang <q710370@gmail.com>
2019-11-23 15:53:07 +0800

    Add registeration feature

--- routes/user.js
+++ routes/user.js
@@ -15,7 +15,31 @@ router.get('/register', (req, res) => {
 })
 //註冊請求
 router.post('/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, password2 } = req.body
+  User.findOne({ email: email }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      res.render('register', {
+        name,
+        email,
+        password,
+        password2
+      })
+    } else {
+      //若不存在
+      const newUser = new User({
+        name,
+        email,
+        password
+      })
+      newUser
+        .save()
+        .then(user => {
+          res.redirect('/')
+        })
+        .catch(err => console.log(err))
+    }
+  })
 })
 //登出
 router.get('/logout', (req, res) => {
